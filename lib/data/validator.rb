814439aa0bc77c7ac47457043f6922f95eae5a96
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2018-11-28 10:23:45 +0900

    Merge pull request #6 from shyouhei/suppress
    add lib/data.rb for future deletion of ::Data

--- lib/data/validator.rb
+++ lib/data/validator.rb
@@ -20,6 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+require_relative '../data'
 Data::Validator = Class.new  # @!parse class Data::Validator; end
 require_relative "validator/version"
 
